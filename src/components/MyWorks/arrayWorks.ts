54923224f856c4f94c296465bc20685dcba802ae
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-01-23 21:50:52 +0200

    myWorks adaptive

--- src/components/MyWorks/arrayWorks.ts
+++ src/components/MyWorks/arrayWorks.ts
@@ -15,7 +15,7 @@ export interface iArrayWorks {
 const arrayWorks: iArrayWorks[] = [
     { id: 1, works: bookshare, title: "Обмен книгами", link: "https://goncharenkovitaliy.github.io/BookShare/",},
     {id: 2, works: collaps, title: "Эффект выпадения", link: "https://goncharenkovitaliy.github.io/Collapse/"},
-    {id: 3, works: github, title: "Поиск GitHup репозитория", link: "https://goncharenkovitaliy.github.io/SearcheGithubRepository/"},
+    {id: 3, works: github, title: "Поиск GitHup", link: "https://goncharenkovitaliy.github.io/SearcheGithubRepository/"},
     {id: 4, works: pogoda, title: "Погода быстро", link: "https://goncharenkovitaliy.github.io/weather-forecast/"},
     {id: 5, works: ticTacToe, title: "Игра крестики нолики", link: "https://goncharenkovitaliy.github.io/TicTacToe/"},
     {id: 6, works: posts, title: "Список постов", link: "https://goncharenkovitaliy.github.io/List-of-posts/"},
--- src/components/News/Post/Post.tsx
+++ src/components/News/Post/Post.tsx
@@ -2,21 +2,14 @@ import MyA from "../../UI/MyA/MyA";
 import { iArrayNews } from "../arrayNews";
 import styles from "./Post.module.css";
 
-export default function Post({
-  image,
-  sabtitle,
-  date,
-  text,
-  link,
-  target,
-}: iArrayNews) {
+export default function Post({ ...posts }: iArrayNews) {
   return (
     <>
-      <img src={image} alt="" className={styles.photo} />
-      <h2 className={styles.subtitle}>{sabtitle}</h2>
-      <p className={styles.date}>{date}</p>
-      <p className={styles.date}>{text}</p>
-      <MyA target={target} link={link} active={true}>
+      <img src={posts.image} alt="" className={styles.photo} />
+      <h2 className={styles.subtitle}>{posts.sabtitle}</h2>
+      <p className={styles.date}>{posts.date}</p>
+      <p className={styles.date}>{posts.text}</p>
+      <MyA target={posts.target} link={posts.link} active={true}>
         Узнать
       </MyA>
     </>
